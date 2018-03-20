a2d21ed1ccfa88b16e41d6910b5ef52db57c4b6f
Kavithakoliparthi <koliparthik@gmail.com>
2018-03-20 19:04:20 +0530

    Test

---
+++ Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProgram1
+{
+    class Client
+    {
+        static public void Main(string[] Args)
+        {
+            TcpClient socketForServer;
+            try
+            {
+                socketForServer = new TcpClient("localHost", 10);
+            }
+            catch
+            {
+                Console.WriteLine("Failed to connect to server at {0}:999", "localhost");
+                return;
+            }
+
+            NetworkStream networkStream = socketForServer.GetStream();
+            StreamReader streamReader = new StreamReader(networkStream);
+            StreamWriter streamWriter = new StreamWriter(networkStream);
+            Console.WriteLine("This is client program who is connected to localhost on port No:10");
+
+            try
+            {
+                Console.WriteLine("enter a string:");
+                string str = Console.ReadLine();
+                while (str != "exit")
+                {
+                    streamWriter.WriteLine(str);
+                    streamWriter.Flush();
+                    Console.WriteLine("type:");
+                    str = Console.ReadLine();
+                }
+                if (str == "exit")
+                {
+                    streamWriter.WriteLine(str);
+                    streamWriter.Flush();
+
+                }
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception reading from Server");
+            }
+            networkStream.Close();
+            Console.WriteLine("Press any key to exit from client program");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetworkProgram
+{
+    class Server
+    {
+        static TcpListener tcpListener = new TcpListener(10);
+        static void Listeners()
+        {
+
+            Socket socketForClient = tcpListener.AcceptSocket();
+            if (socketForClient.Connected)
+            {
+                Console.WriteLine("Client:" + socketForClient.RemoteEndPoint + " now connected to server.");
+                NetworkStream networkStream = new NetworkStream(socketForClient);
+                StreamWriter streamWriter = new StreamWriter(networkStream);
+                StreamReader streamReader = new StreamReader(networkStream);
+
+                while (true)
+                {
+                    string theString = streamReader.ReadLine();
+                    string s = Convert.ToString(theString);
+                    Console.WriteLine("Client X" + s + "clients");
+                }
+                streamReader.Close();
+                networkStream.Close();
+                streamWriter.Close();
+            }
+            socketForClient.Close();
+            Console.WriteLine("Press any key to exit from server program");
+            Console.ReadKey();
+
+        }
+        public static void Main()
+        {
+
+            tcpListener.Start();
+            Console.WriteLine("This is Server program");
+            Console.WriteLine("How many clients are going to connect to this server?:");
+            int numberOfClientsYouNeedToConnect = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfClientsYouNeedToConnect; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(Listeners));
+                newThread.Start();
+            }
+            Console.WriteLine("enter a string:");
+            string str = Console.ReadLine();
+
+        }
+    }
+}
+ 
